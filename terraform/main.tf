6254cbe67d9899edceeb90e36ae12ed0b6d652f2
ondras99 <andrei.retegan166@gmail.com>
2026-05-03 18:56:52 +0300

    changed terraform resource names

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,25 @@
-resource "aws_s3_bucket" "test_bucket" {
+resource "aws_s3_bucket" "licenta_bucket" {
     bucket="licentatestterraformbucket"
 }
-resource "aws_key_pair" "test_key_pair" {
-    key_name=var.test_key_name
+resource "aws_key_pair" "licenta_key_pair" {
+    key_name=var.licenta_key_name
     public_key=file("${path.module}/test_key.pub")
 }
 
-resource "aws_vpc" "test_vpc" {
-    cidr_block=var.test_vpc_cidr_block
+resource "aws_vpc" "licenta_vpc" {
+    cidr_block=var.licenta_vpc_cidr_block
 }
 
 resource "aws_security_group" "acces_ssh" {
     name="Accepta SSH"
     description="Accepta acces SSH (port 22)"
-    vpc_id=aws_vpc.test_vpc.id
+    vpc_id=aws_vpc.licenta_vpc.id
 }
 
 resource "aws_security_group" "acces_http" {
     name="Accepta HTTP"
     description="Accepta acces HTTP (port 80)"
-    vpc_id=aws_vpc.test_vpc.id
+    vpc_id=aws_vpc.licenta_vpc.id
 }
 
 resource "aws_vpc_security_group_ingress_rule" "accepta_ssh" {
@@ -44,51 +44,51 @@ resource "aws_vpc_security_group_ingress_rule" "accepta_http" {
     to_port=80
 }
 
-resource "aws_subnet" "test_subnet" {
-    vpc_id=aws_vpc.test_vpc.id
-    cidr_block=var.test_vpc_cidr_block
+resource "aws_subnet" "licenta_subnet" {
+    vpc_id=aws_vpc.licenta_vpc.id
+    cidr_block=var.licenta_vpc_cidr_block
     map_public_ip_on_launch = true
 }
 
-resource "aws_internet_gateway" "test_gateway" {
-    vpc_id=aws_vpc.test_vpc.id
+resource "aws_internet_gateway" "licenta_gateway" {
+    vpc_id=aws_vpc.licenta_vpc.id
 }
 
-resource "aws_route_table" "test_public_route_table" {
-    vpc_id=aws_vpc.test_vpc.id
+resource "aws_route_table" "licenta_public_route_table" {
+    vpc_id=aws_vpc.licenta_vpc.id
 }
 
-resource "aws_route" "test_public_route" {
-    route_table_id=aws_route_table.test_public_route_table.id
+resource "aws_route" "licenta_public_route" {
+    route_table_id=aws_route_table.licenta_public_route_table.id
     destination_cidr_block="0.0.0.0/0"
-    gateway_id=aws_internet_gateway.test_gateway.id
+    gateway_id=aws_internet_gateway.licenta_gateway.id
 }
 
-resource "aws_route_table_association" "test_association" {
-  subnet_id=aws_subnet.test_subnet.id
-  route_table_id=aws_route_table.test_public_route_table.id
+resource "aws_route_table_association" "licenta_association" {
+  subnet_id=aws_subnet.licenta_subnet.id
+  route_table_id=aws_route_table.licenta_public_route_table.id
 }
 
-resource "aws_instance" "licenta_test_VMRunner" {
-    ami=var.test_ami
-    instance_type=var.test_runner_instance
+resource "aws_instance" "licenta_VMRunner" {
+    ami=var.licenta_ami
+    instance_type=var.runner_instance
     tags={
-        Name=var.test_runner_name
-        description=var.test_runner_desc
+        Name=var.runner_name
+        description=var.runner_desc
     }
     vpc_security_group_ids=[aws_security_group.acces_ssh.id]
-    key_name=aws_key_pair.test_key_pair.key_name
-    subnet_id=aws_subnet.test_subnet.id
+    key_name=aws_key_pair.licenta_key_pair.key_name
+    subnet_id=aws_subnet.licenta_subnet.id
 }
 
-resource "aws_instance" "licenta_test_VMapp" {
-    ami=var.test_ami
-    instance_type=var.test_instance_type
+resource "aws_instance" "licenta_VMapp" {
+    ami=var.licenta_ami
+    instance_type=var.licenta_instance_type
     tags={
-        Name=var.test_app_name
-        description=var.test_app_desc
+        Name=var.app_name
+        description=var.app_desc
     }
     vpc_security_group_ids=[aws_security_group.acces_ssh.id, aws_security_group.acces_http.id]
-    key_name=aws_key_pair.test_key_pair.key_name
-    subnet_id=aws_subnet.test_subnet.id
+    key_name=aws_key_pair.licenta_key_pair.key_name
+    subnet_id=aws_subnet.licenta_subnet.id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,36 @@
-variable "test_key_name" {
+variable "licenta_key_name" {
     type=string
     default="test_key"
 }
-variable "test_vpc_cidr_block" {
+variable "licenta_vpc_cidr_block" {
     type=string
     default="192.168.0.0/16"
 }
-variable "test_ami" {
+variable "licenta_ami" {
     type=string
     default="ami-080254318c2d8932f"
 }
-variable "test_instance_type" {
+variable "licenta_instance_type" {
     type=string
     default="t3.micro"
 }
-variable "test_runner_name" {
+variable "runner_name" {
     type=string
     default="Test Runner VM"
 }
-variable "test_runner_desc" {
+variable "runner_desc" {
     type=string
     default="Creat cu terraform, va fi folosit ca si self hosted runner"
 }
-variable "test_app_name" {
+variable "app_name" {
     type=string
     default="Test App Container VM"
 }
-variable "test_app_desc" {
+variable "app_desc" {
     type=string
     default="Creat cu terraform, va fi folosit pentru a rula aplicatia"
 }
-variable "test_runner_instance" {
+variable "runner_instance" {
     type=string
     default="t3.small"
 }
