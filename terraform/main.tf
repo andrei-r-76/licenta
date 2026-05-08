67bd553129226cb0359c8d84f4c6ab9f17528061
ondras99 <andrei.retegan166@gmail.com>
2026-05-08 17:04:51 +0300

    fix again

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,9 +20,9 @@ resource "aws_vpc_security_group_ingress_rule" "porturi" {
     count=length(var.ports)
     security_group_id = aws_security_group.acces.id
     cidr_ipv4="0.0.0.0/0"
-    from_port=var.ports(count.index)
+    from_port=var.ports[count.index]
     ip_protocol="tcp"
-    to_port=var.ports(count.index)
+    to_port=var.ports[count.index]
 }
 
 resource "aws_vpc_security_group_egress_rule" "accepta_outbound" {
